1e107e6c2140221219cabc24378f80884457f204
hugosique <otahugo02@gmail.com>
2025-06-14 03:33:38 -0300

    DONE - Delete item and sweetalert lib

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import Swal from 'sweetalert2';
 
 // Components
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
@@ -9,6 +10,7 @@ import { IListItems } from '../../interface/IListItems.interface';
 
 // ENUM
 import { ELocalStorage } from '../../enum/ELocalStorage.enum';
+
 
 @Component({
   selector: 'app-list',
@@ -29,7 +31,7 @@ export class ListComponent {
 
   #updateLocalStorage() {
     return localStorage.setItem(
-      ELocalStorage.MY_LIST, 
+      ELocalStorage.MY_LIST,
       JSON.stringify(this.#setListItems())
     );
   }
@@ -92,17 +94,43 @@ export class ListComponent {
   }
 
   public deleteItem(id: string) {
-    this.#setListItems.update((oldValue: IListItems[]) => {
-      return oldValue.filter(el => {
-        el.id !== id;
-      });
-    });
+    Swal.fire({
+      title: "Deletar item?",
+      text: "Você não poderá reverter isso!",
+      // icon: "warning",
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      // confirmButtonColor: "#3085d6",
+      // cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, deletar item"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: IListItems[]) => {
+          return oldValue.filter(el => {
+            return el.id !== id;
+          });
+        });
 
-    return this.#updateLocalStorage();
+        return this.#updateLocalStorage();
+      };
+    });
   }
 
   public deleteAllItems() {
-    localStorage.removeItem(ELocalStorage.MY_LIST);
-    return this.#setListItems.set(this.#parseItems());
+    Swal.fire({
+      title: "Deletar todos os items?",
+      text: "Você não poderá reverter isso!",
+      // icon: "warning",
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      // confirmButtonColor: "#3085d6",
+      // cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, deletar todos"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem(ELocalStorage.MY_LIST);
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
   }
 }

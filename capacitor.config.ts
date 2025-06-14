41d08e359a4324c7a5b8a6a7c53ede85117871a1
hugosique <otahugo02@gmail.com>
2025-06-14 19:15:06 -0300

    DONE - IONIC install

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.todo-list-offline.app',
+  appName: 'todo-list-offline',
+  webDir: 'www'
+};
+
+export default config;
